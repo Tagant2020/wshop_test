6c081051a650b3eafb07cf70a346bbd773b75bf6
Franck2020 <boreltagant@gmail.com>
2024-03-05 02:58:25 +0100

    first push

---
+++ wshop_resolution_erreur/templates/product/index.tpl.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>FW TEST - ACCUEIL</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  <!-- Custom styles for this template -->
+  <link href="css/main.css" rel="stylesheet">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="js/ajax.js" async></script>
+
+</head>
+
+<body>
+    <h2 class='alert alert-success'><center>Bienvenu sur ce site !!</center><h2>
+
+    <div style='width:20%;margin-top:8em;margin-left:40%'>
+        <a href="product_list.php" class="btn btn-info">Voir la liste des produits</a>
+    </div>
+</body>
+
+</html>
